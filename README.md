f53a419602069c3f4f35a9f400e338d9c7ad817b
developper <backend@temp.com>
2023-08-21 14:26:16 +0000

    commit

---
+++ README.md
@@ -0,0 +1,5 @@
+DB
+
+ユーザー名　hirasawa
+アドレス hirasawaworksub@gmail.com
+pass abcd123
--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Category , TopTask , MiddleTask , BottomTask 
+from accounts.models import CustomUser
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        list_display = ("id" , "title")
+        list_display_links = ("id" , "title")
+        
+class TopTaskAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        list_display = ("id" , "title")
+        list_display_links = ("id" , "title")
+        
+class MiddleTaskAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        list_display = ("id" , "title")
+        list_display_links = ("id" , "title")
+        
+
+class BottomTaskAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        list_display = ("id" , "title")
+        list_display_links = ("id" , "title")
+        
+
+admin.site.register(Category , CategoryAdmin)
+admin.site.register(TopTask , TopTaskAdmin)
+admin.site.register(MiddleTask , MiddleTaskAdmin)
+admin.site.register(BottomTask , BottomTaskAdmin)

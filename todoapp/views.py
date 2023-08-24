840bf29c9d59582d62af629078285f0f2698eb86
developper <backend@temp.com>
2023-08-24 14:59:26 +0000

    ページネーション

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render
 from django.views.generic import  ListView  , DetailView
 from .models import MiddleTask , BottomTask
-
+from django.core.paginator import Paginator
 class IndexView(ListView):
     template_name="index.html"
-    context_object_name="middle_task_list"
-    model = MiddleTask
+    paginate_by=5
+    
+    model = BottomTask
     # 単一のビューで複数表示
     def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
+    
+        # コンテクスト拡張
         context.update({
-            'bottom_task_list': BottomTask.objects.order_by(),
+            'bottom_task_list': BottomTask.objects.order_by("parent_task__id"),
+            "middle_task_list": MiddleTask.objects.order_by("id")
+        })
+    # ページネーター指定
+        paginator = Paginator(context['bottom_task_list'], self.paginate_by)
+        page_number = self.request.GET.get('page' ,1 )
+        page_obj = paginator.page(page_number)
+        
+        context.update({
+            'page_obj': page_obj,
+            'paginator': paginator,
         })
         return context
-    
-    def get_queryset(self):
-        return MiddleTask.objects.order_by("delivery")
+
     
 class TaskDetail(DetailView):
     template_name="post.html"
